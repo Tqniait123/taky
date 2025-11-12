e89b345a2c1e48bb55c47c933bc37c0fa21eb7e5
a7medbehiry <a7medbehiry@gmail.com>
2025-11-12 17:14:11 +0200

    refactor [build][apk]

--- lib/core/notifications/notification_service.dart
+++ lib/core/notifications/notification_service.dart
@@ -1,6 +1,7 @@
 
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -255,11 +256,27 @@ class NotificationService {
     Map<String, dynamic>? data,
   }) async {
     try {
-      final serviceAccountJson = await rootBundle.loadString(
-        'assets/service_account.json',
-      );
-      final serviceAccountData =
-          jsonDecode(serviceAccountJson) as Map<String, dynamic>;
+      // final serviceAccountJson = await rootBundle.loadString(
+      //   'assets/service_account.json',
+      // );
+      // final serviceAccountData =
+      //     jsonDecode(serviceAccountJson) as Map<String, dynamic>;
+
+           final serviceAccountEnv = const String.fromEnvironment('FIREBASE_SERVICE_ACCOUNT', defaultValue: '');
+Map<String, dynamic> serviceAccountData;
+
+if (serviceAccountEnv.isNotEmpty) {
+  serviceAccountData = jsonDecode(serviceAccountEnv) as Map<String, dynamic>;
+} else {
+  // fallback (optional)
+  final env = Platform.environment['FIREBASE_SERVICE_ACCOUNT'];
+  if (env != null && env.isNotEmpty) {
+    serviceAccountData = jsonDecode(env) as Map<String, dynamic>;
+  } else {
+    throw Exception('❌ FIREBASE_SERVICE_ACCOUNT env not found.');
+  }
+}
+
 
       final scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
       final credentials = ServiceAccountCredentials.fromJson(
